2b0583e02bdd2d5a7ba7657e6569f2e4e198d205
chenyispace <chrischenyi0060@gmail.com>
2021-06-30 09:23:45 +0800

    'message'

--- src/com/example/ch1/PoolPuzzleOne.java
+++ src/com/example/ch1/PoolPuzzleOne.java
@@ -1,2 +1,36 @@
-package com.example.ch1;public class PoolPuzzleOne {
+package com.example.ch1;
+
+/*
+     Ch 1 - Practice 3: Print out below words
+     a noise
+     annoys
+     an oyster
+*/
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 4) {
+            System.out.print("a");    // Common 1
+            if(x < 1) { // GIVEN
+                System.out.print(" ");  // Diff 1
+            }
+            System.out.print("n");      // Common 2
+
+            if(x > 1) {
+                System.out.print(" oyster");    // Diff 3
+                x = x + 2;  // Diff 3
+            }
+
+            if(x == 1) { // GIVEN
+                System.out.print("noys");   // Diff 2
+            }
+
+            if(x < 1) {
+                System.out.print("oise");   // Diff 1
+            }
+            System.out.println(""); // GIVEN
+            x = x + 1;              // Common 3
+        }
+    }
 }
